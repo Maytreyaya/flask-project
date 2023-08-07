e8c1f75f567133aea8430d62c4209cf597e46846
Maksym Nikityuk <maksym71417@gmail.com>
2023-08-07 23:15:15 +0300

    flask app created, requirements.txt created

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    return "Hallo"
+
+
+if __name__ == '__main__':
+    app.run()
